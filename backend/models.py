bc398a7eae0aa8f9a1195b1fe989f1565d4ab569
samv-33 <psamuelyl@gmail.com>
2025-02-25 20:32:20 -0800

    created models.py

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from firestore_config import db
+
+class User:
+    def __init__(self, name, email, plan):
+        self.name = name
+        self.email = email
+        self.plan = plan
+
+
+    def add_user(self):
+        user_ref = db.collection("users").add({
+            "name": self.name,
+            "email": self.email,
+            "plan": self.plan
+        })
+        return user_ref
+    
+    
+    
+
+
