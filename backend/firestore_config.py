69f3b217ffc490ff59b02b0534481b9e9b54d53b
samvo-csuf <samvo@csu.fullerton.edu>
2025-03-30 16:16:12 -0700

    firestore config update

--- backend/firestore_config.py
+++ backend/firestore_config.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 import firebase_admin
 
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
+import firebase_admin.auth
 
 load_dotenv()
 
@@ -14,7 +15,11 @@ cred = credentials.Certificate(firebase_private_key_path)
 app = firebase_admin.initialize_app(cred)
 
 
-#Get Firestore client
+# Get Firestore client
 db = firestore.client(app=app)
+
+# Get Firestore auth client
+auth = auth.Client(app=app)
+
 
 print("Firebase initialized successfully!")
