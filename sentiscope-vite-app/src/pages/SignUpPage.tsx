2b72b8f88a35a39e247c4f1e0ca087f0ee6c9fc0
samv-33 <psamuelyl@gmail.com>
2025-04-25 18:02:44 -0700

    update sign up page

--- sentiscope-vite-app/src/pages/SignUpPage.tsx
+++ sentiscope-vite-app/src/pages/SignUpPage.tsx
@@ -46,24 +46,24 @@ return (
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label>Add your name:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
+          <label>Add your name: </label>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter a name..." required/>
         </div>
         <div className="form-group">
-          <label>Add your email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+          <label>Add your email: </label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter an email..."  required/>
         </div>
         <div className="form-group">
-          <label>Create a password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+          <label>Create a password: </label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter a password..."  required/>
         </div>
-        <div className="form-group">
-          <label>Subscription Plan:</label>
-          <select value={plan} onChange={(e) => setPlan(e.target.value)}>
-            <option value="free">Free</option>
-            <option value="premium">Premium</option>
-          </select>
-        </div>
+        {/*<div className="form-group">
+        /*  <label>Subscription Plan: </label>
+        /*  <select value={plan} onChange={(e) => setPlan(e.target.value)}>
+        /*    <option value="free">Free</option>
+        /*    <option value="premium">Premium</option>
+        /*  </select>
+        /*</div>*/}
         <button type="submit">Sign Up</button>
       </form>
     </div>
