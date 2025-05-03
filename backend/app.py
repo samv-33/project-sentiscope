4e3a0dce785b989d458cdc8ca9e43dea7556aabf
andrewcesario1 <63276479+andrewcesario1@users.noreply.github.com>
2025-05-03 03:06:53 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask_cors import CORS
-
+from firestore_config import db
 from routes import init_routes
 from classification.server import model_bp
 
@@ -9,10 +10,13 @@ def create_app():
     CORS(app)
 
     init_routes(app)
+
     app.register_blueprint(model_bp)
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000)
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
